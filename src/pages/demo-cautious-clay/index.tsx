7ec199bb69e12ccc833824acd02b578763f97257
DOGHOUSETV <arubigeorge0@gmail.com>
2025-04-04 14:07:32 -0400

    onboard cautious clay

---
+++ src/pages/demo-cautious-clay/index.tsx
@@ -0,0 +1,51 @@
+// ** React/Next.js Imports
+import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
+import React, { useEffect } from "react";
+
+// ** Custom Components, Hooks, Utils, etc.
+import { useSpotify } from "@/context/SpotifyContext";
+import type { ArtistCatalog } from "@/types/catalog";
+import { getArtistCatalog } from "@/utils/get-artist-catalog";
+import AboutSection from "@/views/AboutSection";
+import CardSection from "@/views/CardSection";
+import WelcomeSection from "@/views/WelcomeSection";
+
+export const getServerSideProps: GetServerSideProps<
+  ArtistCatalog
+> = async () => {
+  const artistConfig = {
+    artistId: "6iWuBN32BqCJAeXW6o3nil",
+    artistName: "Cautious Clay",
+  };
+
+  return {
+    props: await getArtistCatalog(artistConfig),
+  };
+};
+
+const CautiousClayDemo = ({
+  items = [],
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const { setArtistCatalog } = useSpotify();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedData: ArtistCatalog = { items: items || [] };
+      setArtistCatalog(fetchedData);
+    };
+
+    fetchData();
+  }, [items, setArtistCatalog]);
+
+  return (
+    <main>
+      <div className='snap-y snap-mandatory h-screen w-screen overflow-scroll scrollbar-hide overflow-x-hidden'>
+        <WelcomeSection />
+        <AboutSection />
+        <CardSection />
+      </div>
+    </main>
+  );
+};
+
+export default CautiousClayDemo;
